6878ecdf8e5d0e032b5292674002c31e5df0bcec
Prashant Gonga <prashant.gonga@gmail.com>
2017-11-01 09:26:00 +0530

    Create buggy0.c

---
+++ week2/buggy0.c
@@ -0,0 +1,6 @@
+//This will generate a bug which will not run the program because the #include has not been declared before starting the program
+
+int main(void)
+{
+    printf("Hello, World!\n");
+}
